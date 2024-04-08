c605b6fd7cc6362eef7fb750140f768a342dcf56
Savannah-Rose <143096367+Savannah-Rose@users.noreply.github.com>
2024-04-08 19:07:49 -0400

    Add files via upload

--- Landmark.java
+++ Landmark.java
@@ -6,32 +6,55 @@ public class Landmark extends Location
 {
 	
 	// array list for landmark names and distances here
-	static ArrayList<String> landmarkNames= new ArrayList<String>();
-	static ArrayList<String> landmarkDistance= new ArrayList<String>();
+	ArrayList<String> landmarkNames = new ArrayList<String>();
+	ArrayList<String> landmarkDistance = new ArrayList<String>();
 	
 	private static final String[] namedLandmarks = {"Independence","Kansas River Crossing", 
 			"Big Blue River Crossing","Fort Kearney", "Chimney Rock"}; 
 	private static final int[] distanceToLandmarks = {0, 102, 82, 118, 250}; // distance to landmark
+	
+	private int dialogueCounter = 0; // Counter to cycle through dialogues
+	private final String[] dialogues = 
+		{
+		    "Make sure you have extras of everything in case something happens.",
+		    "Be careful when crossing the river, especially the deeper kind.",
+		    "Trading is often far cheaper than buying at forts, but you never know what you'll get."
+		};
+	
+	/**
+	 * 
+	 * Initialize the landmark information within a constructor
+	 * 
+	 */
+	public Landmark()
+	{
+		// Storing namedLandmarks into landmarkNames ArrayList
+	    for (String landmark : namedLandmarks) {
+	        landmarkNames.add(landmark);
+	    }
 
+	    // Storing distanceToLandmarks into landmarkDistance ArrayList
+	    for (int distance : distanceToLandmarks) {
+	        landmarkDistance.add(String.valueOf(distance)); // Convert int to String
+	    }
+	}
+	
 	/**
 	 * Finds distance from each landmark from current location (found in superclass)
 	 * @return the total distance from the landmark compared to current distance
 	 * 
 	 */
-	public static int distanceFrom()
-	{
-		// add in inheritance from superclass
-		int currentDistance = 0;
-		
-	    for (int distance : distanceToLandmarks) 
-	    {
-	        if (distance > currentDistance) 
-	        {
-	            return distance - currentDistance;
-	        }
-	        currentDistance += distance;
-	    }
-	    return 0; // No more landmarks
+	public int distanceFrom()
+	{   
+		int currentDistance = getTotalTraveled(); // Use getTotalTraveled from superclass
+
+        for (int distance : distanceToLandmarks) {
+            if (distance > currentDistance) {
+                return distance - currentDistance;
+            }
+            currentDistance += distance;
+        }
+        return 0; // No more landmarks
 	}
 	
 	/**
@@ -41,11 +64,13 @@ public class Landmark extends Location
 	 * 
 	 */
 	
-	public static String getLandmarkName(String selectedLandmark)
+	public String getLandmarkName(String selectedLandmark)
 	{
-		
-		
-		return "Error - Location cannot be found."; // if landmark name not found
+		int index = landmarkNames.indexOf(selectedLandmark);
+        if (index != -1) {
+            return landmarkNames.get(index);
+        }
+        return "Error - Location cannot be found."; // if landmark name not found
 	}
 	
 	/**
@@ -55,11 +80,11 @@ public class Landmark extends Location
 	 * @return true if landmark is a fort, false otherwise
 	 * 
 	 */
-	public static boolean isFort(int location, int landmarkType)
+	public boolean isFort(int location, int landmarkType)
 	{
-		
-		
-		return false; // if not fort - default
+        String landmark = landmarkNames.get(location);
+        
+        return landmark.contains("Fort"); // Simple check if landmark name contains "Fort"
 	}
 	
 	/**
@@ -69,24 +94,24 @@ public class Landmark extends Location
 	 * @return true if currently at landmark, false if otherwise
 	 * 
 	 */
-	public static boolean atLandmark(int distance)
+	public boolean atLandmark(int distance)
 	{
-		int currentDistance = 0;
-		
-		if (distance == currentDistance)
-		{
-			// locationInstance.nextDay(); 
-		}
-		return false;
+		int currentDistance = getTotalTraveled(); // Use getTotalTraveled from superclass
+        return distance == currentDistance;
 	}
 	
 	
 	/**
 	 * Displays landmark surroundings upon selection
 	 */
-	public static void lookAroundOption()
+	public void lookAroundOption()
 	{
+		String landmarkName = landmarkNames.get(0);
+		String currentDate = DateString(); // get current date from superclass
 		
+		 System.out.println("[Photo Placeholder]");
+		 System.out.println("You are at " + landmarkName + ".");
+		 System.out.println("Today's date is " + currentDate + ".");
 	}
 	
 	/**
@@ -94,10 +119,13 @@ public class Landmark extends Location
 	 * @param dialogueType - selects type of dialogue displayed
 	 * @return index number of dialogue, otherwise return 0 
 	 */
-	public static int talkToOption(int dialogueType)
+	public int talkToOption(int dialogueType)
 	{
-		// use Array or ArrayList to store dialog?
+		System.out.println(dialogues[dialogueCounter]);
 		
-		return 0; // if random fails
+		// Cycle through dialogues
+	    dialogueCounter = (dialogueCounter + 1) % dialogues.length;
+		
+	    return dialogueCounter;
 	}
 }
